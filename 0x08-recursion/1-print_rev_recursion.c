5cdec0e5297f1a6059d3cd56f5933866956d1c1a
EduUgo <eduugo8@gmail.com>
2022-06-22 11:54:57 -0700

    My first 1 commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,4 @@
-i#include "main.h"
+#include "main.h"
 /**
  *_print_rev_recursion - prints a string in reverse
  *@s: pointer block of memory to fill
@@ -9,7 +9,7 @@ void _print_rev_recursion(char *s)
 {
 	if (*s) /*If statement*/
 	{
-	_print_rev_recursion(s + 1); /*add s*/
-	_putchar(*s); /*print s*/
+		_print_rev_recursion(s + 1); /*add s*/
+		_putchar(*s); /*print s*/
 	}
 }
